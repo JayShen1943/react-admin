0eab35d94f630d8aa69752320745f6186b69609a
jay <80699827@qq.com>
2021-07-22 07:36:07 +0800

    feat:首次上传

--- src/components/privateRouter/index.js
+++ src/components/privateRouter/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: JayShen
+ * @Date: 2021-02-03 13:12:00
+ * @LastEditors: JayShen
+ * @LastEditTime: 2021-02-03 13:57:51
+ * @Description: 
+ */
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 //获取token
@@ -9,7 +16,7 @@ const PrivateRouter = ({ component: Component, ...rest }) => {
       {...rest}
       render={
         (routePrpos) =>
-          getToken() ? <Component {...routePrpos} /> : <Redirect to="/" /> //判断token，是否重定向到登录页
+          !getToken() ? <Component {...routePrpos} /> : <Redirect to="/" /> //判断token，是否重定向到登录页
       }
     />
   );
